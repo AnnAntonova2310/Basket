5dbaacbc560ed499f3c26586d895afca339a41a3
AnnAntonova2310 <anyashaida@mail.ru>
2022-10-02 10:04:20 +0300

    onClick

--- src/Components/counter.tsx
+++ src/Components/counter.tsx
@@ -5,15 +5,19 @@ const Counter = () => {
     const formatCount = () => {
         return count === 0 ? 'empty' : count
     }
-    const getBageClasses = () => {
+    const getBadgeClasses = () => {
         let classes = 'badge m-2 '
         classes += count === 0 ? 'bg-warning' : 'bg-primary'
         return classes
     }
+    const handlerIncrement = () => {
+        count = count + 1;
+        console.log(count)
+    }
     return (
         <>
-            <span className={getBageClasses()}>{formatCount()}</span>
-            <button className={'btn bg-primary m-2'}>+</button>
+            <span className={getBadgeClasses()}>{formatCount()}</span>
+            <button className={'btn bg-primary m-2 btn-hover'} onClick={handlerIncrement}>+</button>
         </>
     );
 };
