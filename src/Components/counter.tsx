bab1b495d0ad0b25fa0c0e02a07886ebfd501e75
AnnAntonova2310 <anyashaida@mail.ru>
2022-10-02 09:28:22 +0300

    Dinamic classes

--- src/Components/counter.tsx
+++ src/Components/counter.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
 const Counter = () => {
-    let count=0;
-    const formatCount =()=> {
+    let count = 0;
+    const formatCount = () => {
         return count === 0 ? 'empty' : count
     }
-
+    const getBageClasses = () => {
+        let classes = 'badge m-2 '
+        classes += count === 0 ? 'bg-warning' : 'bg-primary'
+        return classes
+    }
     return (
         <>
-            <span className={'badge bg-primary'}>{formatCount()}</span>
-            <button className={'btn bg-primary'}>+</button>
+            <span className={getBageClasses()}>{formatCount()}</span>
+            <button className={'btn bg-primary m-2'}>+</button>
         </>
     );
 };
