5f2eb097b7179ee4e156b882450cd1840ebbb93f
AnnAntonova2310 <anyashaida@mail.ru>
2022-10-02 10:44:05 +0300

    Rendering a list from an array

--- src/Components/counter.tsx
+++ src/Components/counter.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 
 const Counter = () => {
-    const [count, setCount]=useState(0)
+    const [count, setCount] = useState(0)
+    const [tag, setTags] = useState(['tag1', 'tag2', 'tag3'])
     const formatCount = () => {
         return count === 0 ? 'empty' : count
     }
@@ -11,18 +12,30 @@ const Counter = () => {
         return classes
     }
     const handlerIncrement = () => {
-        setCount((prevState)=>prevState+1)
+        setCount((prevState) => prevState + 1)
         console.log(count)
     }
     const handlerDecrement = () => {
-        setCount((prevState)=>prevState-1)
+        setCount((prevState) => prevState - 1)
         console.log(count)
+    }
+    const handlerTagChange = () => {
+        setTags(['tag4', 'tag5'])
     }
     return (
         <>
+            <ul>{
+                tag.map(tag => (
+                    <li
+                        className={'btn bg-green m-2 tag' }
+                        key={tag}
+                        onClick={handlerTagChange}
+                    >{tag}</li>))
+            }
+            </ul>
             <span className={getBadgeClasses()}>{formatCount()}</span>
-            <button className={'btn bg-primary m-2 btn-hover'} onClick={handlerIncrement}>+</button>
-            <button className={'btn bg-primary m-2 btn-hover'} onClick={handlerDecrement}>-</button>
+            <button className={'btn bg-green m-2 btn:hover'} onClick={handlerIncrement}>+</button>
+            <button className={'btn bg-green m-2 btn:hover'} onClick={handlerDecrement}>-</button>
         </>
     );
 };
